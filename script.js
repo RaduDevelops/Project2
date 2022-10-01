ffa0c75289982f7268c0e746509e9c7b9f2d128a
RaduDevelops <raducu_hogas@yahoo.com>
2022-10-02 00:22:34 +0300

    al doilea commit pe 2.10

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-const heloo = "hello world";
-console.log(heloo);
-var myVar = document.getElementsById("feature-sub");
-myVar.addEventListener("click", function(){ myVar.style.display="block";});
+console.log("hello");
